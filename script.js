5911e27290aae1b08e2fd1bf43f51273c63c2bae
Erika Cannon <cannone.rva@gmail.com>
2022-03-16 14:39:36 -0400

    first prompt set up

--- script.js
+++ script.js
@@ -4,6 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var firstPrompt = function() {
+  var passwordLength = prompt("How long would you like your password? \r\nMust have a minimum of 8 characters and a max 128 characters");
+
+  if (passwordLength < 128 && passwordLength >= 8) {
+  //next prompt
+
+} else{
+  alert("This password number does not meet the requirements. Try again!");
+  firstPrompt();
+}
+};
+
+
+firstPrompt();
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
