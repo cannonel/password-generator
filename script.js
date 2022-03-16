e847b56629b84f20611388e5adbdefb6a2a12657
Erika Cannon <cannone.rva@gmail.com>
2022-03-16 15:28:09 -0400

    add prompts for password options

--- script.js
+++ script.js
@@ -4,11 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength ="";
+
 var firstPrompt = function() {
   var passwordLength = prompt("How long would you like your password? \r\nMust have a minimum of 8 characters and a max 128 characters");
 
   if (passwordLength < 128 && passwordLength >= 8) {
-  //next prompt
+  //next prompt asking about lowercase letters
+  var lowercaseYes = confirm("Would you like to include lowercase letters in your password?");
+  var uppercaseYes = confirm("Would you like to include uppercase letters in your password?");
+  var numericalYes = confirm("Would you like to include numbers in your password?");
+  var specialYes = confirm("Would you like to include special characters in your password?");
+
+  
+  var passwordLength = passwordLength;
+  console.log(lowercaseYes)
+  console.log(uppercaseYes)
+  console.log(numericalYes)
+  console.log(specialYes)
+    
 
 } else{
   alert("This password number does not meet the requirements. Try again!");
