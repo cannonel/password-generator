dbdd0084535ec3de1f680ef523919d91014a3ada
Erika Cannon <cannone.rva@gmail.com>
2022-03-20 13:28:18 -0400

    for loop added, generator successful

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var lower = "qwertyuiopasdfghjklzxcvbnm";
-var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
-var num = "123456789";
-var sym = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+generateBtn.addEventListener("click", writePassword);
 
-var firstPrompt = function() {
-  var passwordLength = prompt("How long would you like your password? \r\nMust have a minimum of 8 characters and a max 128 characters");
-
-  if (passwordLength < 128 && passwordLength >= 8) {
-  //next prompt asking about lowercase letters
-  var lowercaseYes = confirm("Would you like to include lowercase letters in your password?");
-  var uppercaseYes = confirm("Would you like to include uppercase letters in your password?");
-  var numericalYes = confirm("Would you like to include numbers in your password?");
-  var specialYes = confirm("Would you like to include special characters in your password?");
-
-  
-  var passwordLength = passwordLength;
-  console.log(lowercaseYes)
-  console.log(uppercaseYes)
-  console.log(numericalYes)
-  console.log(specialYes)
-    
-
-} else{
-  alert("This password number does not meet the requirements. Try again!");
-  firstPrompt();
-}
-};
-
-
-firstPrompt();
-
-
-
-
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+//generate password function start
+function generatePassword() {
+  var lower = "qwertyuiopasdfghjklzxcvbnm";
+  var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  var num = "123456789";
+  var sym = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  var password = "";
+
+  var passwordLength = prompt(
+    "How long would you like your password? \r\nMust have a minimum of 8 characters and a max 128 characters"
+  );
+
+  passwordLength = parseInt(passwordLength);
+  console.log(passwordLength);
+
+  if (passwordLength < 128 && passwordLength >= 8) {
+    //next prompt asking about lowercase letters
+    var lowercaseYes = confirm(
+      "Would you like to include lowercase letters in your password?"
+    );
+    var uppercaseYes = confirm(
+      "Would you like to include uppercase letters in your password?"
+    );
+    var numericalYes = confirm(
+      "Would you like to include numbers in your password?"
+    );
+    var specialYes = confirm(
+      "Would you like to include special characters in your password?"
+    );
+
+
+    for (i = 0; i < passwordLength; i++) {
+      if (password.length < passwordLength) {
+        if (lowercaseYes) {
+          password += upper[Math.floor(Math.random() * lower.length)];
+        }
+        if (uppercaseYes) {
+          password += lower[Math.floor(Math.random() * upper.length)];
+        }
+        if (numericalYes) {
+          password += num[Math.floor(Math.random() * num.length)];
+        }
+        if (specialYes) {
+          password += sym[Math.floor(Math.random() * sym.length)];
+        }
+      } else {
+        break;
+      }
+    }
+    return password;
+  } else {
+    alert("This password number does not meet the requirements. Try again!");
+  }
+}
